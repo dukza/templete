a438e471937ee0f621d3391dad8ca1ac0c2a971d
dukyeon ha <iribbonkr@gmail.com>
2020-11-25 22:22:53 +0900

    sider

--- src/Components/GuideCon.js
+++ src/Components/GuideCon.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import Sider from './Sider';
+import styled from "styled-components";
+
+
+
+
+const ConWrap = styled.div`
+    display:grid;
+    grid-column-gap: 4.8rem;
+    grid-template-columns: repeat(16, 1fr);
+`
+
+const Con = styled.div`
+    grid-column: 5 / span 12;
+    height:14000px;
+`
+
+
+
 
 const GuideCon = ({children}) => {
     return(
         <div className="container mx-auto">
             <Sider></Sider>
-            <div>{children}</div>            
+            <ConWrap>
+               <Con>{children}</Con>   
+            </ConWrap>       
         </div>
     )
 }
--- src/Components/Sider.js
+++ src/Components/Sider.js
@@ -2,17 +2,36 @@ import React from 'react';
 import {Link, withRouter} from "react-router-dom";
 import styled from "styled-components";
 
+const SlinkWraper = styled.div`
+    position: absolute;
+    top:0px;
+    bottom: 0;
+    height: auto;   
+    width: auto; 
+    trasition:transform 0.25s cubic-bezier(0.19, 1, 0.22, 1) 0s;
+`
+
+const SlinkWrap = styled.div`
+    position: sticky; 
+    top:0px;
+    transform: translateY(11rem);
+`
+
 
 const SLink = styled(Link)`
     color: ${props => props.current ? "#276ef1" : "#000"};
     display:block;
+    text-decoration:none;
+    line-height:1.75;
 `
 
 export default withRouter(({location:{pathname}}) => (
-    <div id="sider">
-        <SLink to="/guide/logo" current={pathname==='/guide/logo'}>logo</SLink>
-        <SLink to="/guide/color" current={pathname==='/guide/color'}>color</SLink>
-        <SLink to="/guide/brandArchitecture" current={pathname==='/guide/brandArchitecture'}>Brand Architecture</SLink>
-        <SLink to="/guide/composition" current={pathname==='/guide/composition'}>Composition</SLink>
-    </div>
+    <SlinkWraper>
+        <SlinkWrap>
+            <SLink to="/guide/logo" current={pathname==='/guide/logo'}>logo</SLink>
+            <SLink to="/guide/color" current={pathname==='/guide/color'}>color</SLink>
+            <SLink to="/guide/brandArchitecture" current={pathname==='/guide/brandArchitecture'}>Brand Architecture</SLink>
+            <SLink to="/guide/composition" current={pathname==='/guide/composition'}>Composition</SLink>    
+        </SlinkWrap>
+    </SlinkWraper>
 ))
