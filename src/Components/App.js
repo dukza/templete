503fa52f44a776cb1af9bcccd6f02ecc95d7b6cf
dukyeon ha <iribbonkr@gmail.com>
2021-01-08 18:35:38 +0900

    pouter

--- src/Components/App.js
+++ src/Components/App.js
@@ -26,7 +26,7 @@ const App = () => {
   useEffect(()=>{
     let page = location.pathname;
     trackPage(page);
-    console.log('page',page)
+    console.log('page', page)
   },[location])
   return (
     <>
